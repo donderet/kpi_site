79fad073a06ecba3bc18cf2ec3151ce06f97b4b4
donderet <148077412+donderet@users.noreply.github.com>
2024-05-14 12:14:08 +0300

    Adjusted config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,10 +122,6 @@ const config = {
                 label: 'Blog',
                 to: '/blog',
               },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
             ],
           },
         ],
